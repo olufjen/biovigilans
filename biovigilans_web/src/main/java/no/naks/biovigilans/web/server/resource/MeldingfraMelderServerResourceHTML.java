ca253044460781f9e8e19b2578580259f6bfa8b5
Oluf Jensen <olufjen@gmail.com>
2016-06-27 08:09:50 +0200

    endret utsendende og opplysninger i melding fra melder skjema

--- biovigilans_web/src/main/java/no/naks/biovigilans/web/server/resource/MeldingfraMelderServerResourceHTML.java
+++ biovigilans_web/src/main/java/no/naks/biovigilans/web/server/resource/MeldingfraMelderServerResourceHTML.java
@@ -132,6 +132,12 @@ public class MeldingfraMelderServerResourceHTML extends
 	    	 diskusjon = hendelseWebService.collectDiskusjon(diskId);
 //	    	 String komm = "Vennlingst stryk teksten og skriv ditt svar: " + diskusjon.getKommentar();
 //	    	 diskusjon.setKommentar(komm);
+	    	 String saksbehandler = diskusjon.getTema();
+	    	 char sep = ';';
+	    	 saksbehandler = extractString(saksbehandler, sep, -1);
+	    	 if (saksbehandler == null)
+	    		 saksbehandler = "ukjent";
+	    	 diskusjon.setSaksbehandler(saksbehandler);
 	     }
 	     if (melding == null){
 	    	 melding = new AnnenkomplikasjonImpl("");
@@ -146,6 +152,7 @@ public class MeldingfraMelderServerResourceHTML extends
 	    	 diskusjon = new DiskusjonImpl();
 	    	 diskusjon.setTema("Ingen melding"); 
 	    	 diskusjon.setKommentar("");
+	    	 diskusjon.setSaksbehandler("ukjent");
 	     }
 		 displayPart = "none";
     	 datePart = "none";
